3b7fba7a7b0fa38b4cdd55bb8c9dba709d7084bd
John Brady <jbradysd@icloud.com>
2022-05-11 20:30:44 -0700

    implementing highscores

--- 07-DOM-and-events/script.js
+++ 07-DOM-and-events/script.js
@@ -11,6 +11,7 @@ console.log(document.querySelector('.guess').value); */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
+let highscore = 0
 document.querySelector('.number').textContent = '?'
 
 // Starting the game
@@ -28,6 +29,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!'
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
+
+    if (score > highscore) {
+      highscore = score
+      document.querySelector('.highscore').textContent = highscore
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
