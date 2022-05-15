b5a5cf9d8cc0dde418aeb806f95579fe5c66cabb
John Brady <jbradysd@icloud.com>
2022-05-14 21:55:39 -0700

    using optional chaining (?) with nullish coalescing operator (??)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -60,6 +60,37 @@ const restaurant = {
   },
 }
 
+// without optional chaining
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open)
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open)
+console.log(restaurant.openingHours?.mon?.open)
+
+// Real world example
+const days = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+
+for (const day of days) {
+  // console.log(day)
+  const open = restaurant.openingHours[day]?.open ?? 'closed'
+  console.log(`On ${day}, we open at ${open}`)
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does NOT exist')
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does NOT exist')
+
+// Arrays
+const users = [
+  {
+    name: 'Jonas',
+    email: 'hello@jonas.io',
+  },
+]
+
+console.log(users[0]?.name ?? 'User array empty')
+
 /* 
 // The for-of loop
 // ...the old way
